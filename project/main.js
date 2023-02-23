431d0689bf3d4a369ce6e2047dcee0ad13b5afda
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-02-23 17:48:43 +0530

    Caesar test cases

--- project/main.js
+++ project/main.js
@@ -30,3 +30,55 @@ export const calculator = {
         return a/b;
     }
 }
+
+export const caesar = function(a, b) {
+    let str = a;
+    let n = b;
+    if (n > 26 || n < -26) {
+      n = n % 26
+    }
+    // console.log(`n = ${n}`);
+    let uni = [];
+    for (let i = 0; i < str.length; i++) {
+      uni[i] = str.charCodeAt(i);
+    }
+    for (let i = 0; i < uni.length; i++) {
+    //   console.log(`1st = ${uni[i] >= 65 && uni[i] <= 90}`)
+      if (uni[i] >= 65 && uni[i] <= 90) {
+        // console.log(` 1stif = ${uni[i] + n < 65}`)
+        // console.log(`  u[i] ${uni[i] = 90 - 1 + n}`)
+        if (uni[i] + n < 65) {
+          uni[i] = 91 - (65 - (uni[i] + n));
+        }
+        else if ((uni[i] + n) > 90) {
+          uni[i] = 64 + uni[i] + n - 90;
+        }
+        else {
+          uni[i] = uni[i] + n;
+        }
+        // console.log(`  u[i] ${uni[i]}`)
+  
+      }
+  
+    //   console.log(`2st = ${uni[i] >= 97 && uni[i] <= 122}`)
+      if (uni[i] >= 97 && uni[i] <= 122) {
+  
+        if (uni[i] + n < 97) {
+          uni[i] = 123 - (97 - (uni[i] + n));
+        }
+        else if (uni[i] + n > 122) {
+          uni[i] = 96 + uni[i] + n - 122;
+        }
+        else {
+          uni[i] = uni[i] + n;
+        }
+      }
+  
+    }
+    // console.log(uni);
+    let arr = [];
+    for (let i = 0; i < uni.length; i++) {
+      arr[i] = String.fromCharCode(uni[i]);
+    }
+    return arr.join("");
+};
--- project/main.test.js
+++ project/main.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseStr, calculator } from "./main";
+import { capitalize, reverseStr, calculator, caesar } from "./main";
 
 describe("Capitalize Test cases: ", ()=>{
     test("capitalization of harsha should be Harsha", ()=>{
@@ -55,3 +55,27 @@ describe("Calculator test Cases: ", ()=>{
     });
     
 });
+
+describe("Caesar Test cases: ",()=>{
+    test('works with single letters', () => {
+        expect(caesar('A', 1)).toBe('B');
+      });
+      test('works with words', () => {
+        expect(caesar('Aaa', 1)).toBe('Bbb');
+      });
+      test('works with phrases', () => {
+        expect(caesar('Hello, World!', 5)).toBe('Mjqqt, Btwqi!');
+      });
+      test('works with negative shift', () => {
+        expect(caesar('Mjqqt, Btwqi!', -5)).toBe('Hello, World!');
+      });
+      test('wraps', () => {
+        expect(caesar('Z', 1)).toBe('A');
+      });
+      test('works with large shift factors', () => {
+        expect(caesar('Hello, World!', 75)).toBe('Ebiil, Tloia!');
+      });
+      test('works with large negative shift factors', () => {
+        expect(caesar('Hello, World!', -29)).toBe('Ebiil, Tloia!');
+      });
+})
